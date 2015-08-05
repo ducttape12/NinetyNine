e1554624bf99f5ed4f2c6a5a195ee90073d6cc0c
ducttape12 <ducttape12@yahoo.com>
2015-08-05 21:26:41 +0000

    Layout much better, new music (game music placeholder due to licensing restrictions)

--- scripts/AudioPlayerDirective.js
+++ scripts/AudioPlayerDirective.js
@@ -16,58 +16,48 @@ angular.module('ninetynine').directive('audioPlayer', ['$timeout', function($tim
             audio.volume = 0;
 
             var turnVolumeUp = function() {
-                console.log('In turnVolumeUp.  Volume: ' + audio.volume);
                 if (audio.volume <= 1 - volumeChangeDelta) {
                     audio.volume += volumeChangeDelta;
                     audio.volume = Math.round(audio.volume * 10) / 10;
-                console.log('In turnVolumeUp.  Volume increased to: ' + audio.volume);
                 }
 
 
                 if (audio.volume < 1) {
-                console.log('In turnVolumeUp.  Processing again.');
                     $timeout(turnVolumeUp, volumeChangeDelay);
-                } else {
-                console.log('In turnVolumeUp.  Done.');
+                }
+                else {
                     audio.volume = 1;
                 }
             };
 
             var turnVolumeDown = function() {
-                console.log('In turnVolumeDown.  Volume: ' + audio.volume);
                 if (audio.volume >= volumeChangeDelta) {
                     audio.volume -= volumeChangeDelta;
                     audio.volume = Math.round(audio.volume * 10) / 10;
-                console.log('In turnVolumeDown.  Volume decreased to: ' + audio.volume);
                 }
 
                 if (audio.volume > 0) {
-                console.log('In turnVolumeDown.  Processing again.');
                     $timeout(turnVolumeDown, volumeChangeDelay);
-                } else {
-                console.log('In turnVolumeDown.  Done.');
+                }
+                else {
                     audio.volume = 0;
                     audio.pause();
                 }
             }
 
             scope.$watch('playing()', function(newValue, oldValue) {
-                console.log('Playing changed from ' + oldValue + ' to ' + newValue);
                 if (oldValue != newValue) {
                     if (newValue) {
-                        console.log('playing ' + scope.source());
                         audio.play();
                         turnVolumeUp();
                     }
                     else {
-                        console.log('stopping' + scope.source());
                         turnVolumeDown();
                     }
                 }
             });
 
             if (scope.playing()) {
-                console.log('Initial play of ' + scope.source());
                 audio.play();
                 turnVolumeUp();
             }
