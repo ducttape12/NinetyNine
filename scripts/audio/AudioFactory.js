849ab73216b76af1c1193dc2edab2329bd167245
Keith Ott <kott@mlock.com>
2015-10-22 15:06:16 -0500

    HTML5 audio is implemented

--- scripts/audio/AudioFactory.js
+++ scripts/audio/AudioFactory.js
@@ -1,10 +1,11 @@
-angular.module('ninetynine').factory('AudioFactory', ['IS_CORDOVA', 'CordovaAudioFactory', 'Html5AudioFactory', function(IS_CORDOVA, CordovaAudioFactory, Html5AudioFactory) {
+angular.module('ninetynine').factory('AudioFactory', ['IS_CORDOVA', 'CordovaAudioFactory', 'Html5AudioFactory', '$timeout', function(IS_CORDOVA, CordovaAudioFactory, Html5AudioFactory, $timeout) {
     'use strict';
     
     return {
         create: function(path, playbackFinishCallback) {
             if(!IS_CORDOVA) {
                 return Html5AudioFactory.create(path, playbackFinishCallback);
+                
             } else {
                 // TODO: Create a Cordova audio element
             }
--- scripts/audio/Html5AudioFactory.js
+++ scripts/audio/Html5AudioFactory.js
@@ -1,5 +1,11 @@
-angular.module('ninetynine').factory('Html5AudioFactory', [function() {
+angular.module('ninetynine').factory('Html5AudioFactory', ['$timeout', function($timeout) {
     'use strict';
+    
+    var volumeChangeDelay = 100;
+    var volumeChangeDelta = 0.1;
+    
+    
+    
     
     var Html5Audio = function(path, playbackFinishCallback) {
         this.audio = new Audio(path);
@@ -10,15 +16,56 @@ angular.module('ninetynine').factory('Html5AudioFactory', [function() {
     
     Html5Audio.prototype.play = function() {
         this.audio.play();
+        this.fadeIn();
+    };
+    
+    Html5Audio.prototype.fadeIn = function() {
+        if (this.audio.volume <= 1 - volumeChangeDelta) {
+            this.audio.volume += volumeChangeDelta;
+            this.audio.volume = Math.round(audio.volume * 10) / 10;
+        }
+
+
+        if (this.audio.volume < 1) {
+            $timeout(this.turnVolumeUp, volumeChangeDelay);
+        }
+        else {
+            this.audio.volume = 1;
+        }
     };
     
     Html5Audio.prototype.pause = function() {
-        this.audio.pause();
+        var self = this;
+        this.fadeOut(function() {
+            self.audio.pause();
+        })
     };
     
     Html5Audio.prototype.stop = function() {
-        this.audio.pause();
-        this.audio.currentTime = 0;
+        var self = this;
+        this.fadeOut(function() {
+            self.audio.pause();
+            self.audio.currentTime = 0;
+        })
+    };
+    
+    Html5Audio.prototype.fadeOut = function(fadeCompleteCallback) {
+        var self = this;
+        
+        if (this.audio.volume >= volumeChangeDelta) {
+            this.audio.volume -= volumeChangeDelta;
+            this.audio.volume = Math.round(this.audio.volume * 10) / 10;
+        }
+
+        if (this.audio.volume > 0) {
+            $timeout(function() {
+                self.fadeOut(fadeCompleteCallback);
+            }, volumeChangeDelay);
+        }
+        else {
+            this.audio.volume = 0;
+            fadeCompleteCallback();
+        }
     };
     
     Html5Audio.prototype.setVolume = function(volume) {
